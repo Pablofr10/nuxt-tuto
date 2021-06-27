fffdf13679384cfca0b4a6acd4bc094d4bdd643e
Pablorf10 <rafanet.live@gmail.com>
2021-06-26 22:12:15 -0300

    Paginas

---
+++ pages/paises/_id.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Você está em: País {{ id }}</h1>
+</template>
+
+<script>
+export default {
+  validate ({ params }) {
+    return /^\d+$/.test(params.id)
+  },
+
+  data () {
+    return {
+      id: this.$route.params.id
+    }
+  }
+}
+</script>
---
+++ pages/paises/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Países</h1>
+    <ul>
+      <li v-for="pais in paises" :key="pais.id">
+        <nuxt-link :to="'paises/' + pais.id">
+          {{ pais.nome }}
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      paises: [
+        { id: 1, nome: 'Angola' },
+        { id: 2, nome: 'Brasil' },
+        { id: 3, nome: 'Cabo Verde' },
+        { id: 4, nome: 'Portugal' }
+      ]
+    }
+  }
+}
+</script>
